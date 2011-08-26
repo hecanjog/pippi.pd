052510bc7903011cc2d8e030d49564589fc701dc
Erik Schoster <erik@heythere.org>
2011-08-26 15:44:31 -0500

    more config for console pd hotness

--- pippi.sh
+++ pippi.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 #/usr/bin/jackd -P 70 -r -dalsa -r44100 -p2048 -n2 -D -Chw:0,0 -Phw:0 -Xseq &
-screen pd -nogui -jack -channels 2 -alsamidi -mididev 2 -open ~/code/pippi/test.pd -send "pd dsp 1" 
+screen pd -nogui -jack -channels 2 -alsamidi -mididev 2 -open ~/code/pippi/_pip.pd -send "pd dsp 1" 
