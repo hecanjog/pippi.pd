07b77f831a4e2f2a4195754b4f717a46476716ec
Erik Schoster <erik@heythere.org>
2012-07-28 10:45:59 -0500

    Add global param control and some hooks for pygame playback. Expand shine orc generator to add pulses and glitching

--- console.py
+++ console.py
@@ -2,7 +2,6 @@ import cmd
 import socket
 import subprocess
 import threading
-from multiprocessing import Process, Queue, current_process, freeze_support 
 from pygame import mixer
 import os
 import math
@@ -16,50 +15,102 @@ class Pippi(cmd.Cmd):
     intro = 'Pippi Console'
 
     voices = [] 
+    channels = []
+    stopped = []
     renders = []
+    cc = {} 
 
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.condition = threading.Condition()
-        self.m = Mixer(mixer, self.renders, self.voices, self.condition)
+        self.m = Mixer(mixer, self.renders, self.voices, self.channels, self.condition)
         self.m.start()
 
     def play(self, cmd):
         orcs = os.listdir('orc/')
         for orc in orcs:
             if cmd[0] == orc[0:2]:
+                t = cmd[0]
                 cmd.pop(0)
                 orc = 'orc.' + orc.split('.')[0]
                 p = __import__(orc, globals(), locals(), ['play'])
-                
-                Voice(p.play, cmd, self.renders, self.voices, self.condition).start()
+
+                for c in self.cc:
+                    if c == t or c == 'g':
+                        cmd = cmd + self.cc[c]
+
+                Voice(p.play, t, cmd, self.renders, self.voices, self.condition).start()
 
                 return True
 
         print 'not found'
 
+    def do_s(self, cmd):
+        cmds = cmd.split(' ')
+
+        channel = self.channels[int(cmds[0])]
+        try:
+            channel[0].fadeout(1500 + dsp.randint(0, 500))
+            self.voices.append((channel[0].get_sound(), channel[1], channel[2]))
+        except AttributeError:
+            pass
+
+        self.channels.remove(channel)
+
+    def do_p(self, cmd):
+        cmds = cmd.split(' ')
+
+        sound = self.voices[int(cmds[0])]
+        self.channels.append((sound[0].play(-1), sound[1], sound[2]))
+        self.voices.remove(sound)
+
+        self.do_i()
+
+    def do_c(self, cmd):
+        cmd = cmd.split(' ')
+        t = cmd[0]
+        cmd.pop(0)
+        self.cc[t] = cmd
+
     def do_v(self, cmd):
         cmds = cmd.split(' ')
 
-        voice = self.voices[int(cmds[0])]
+        channel = self.channels[int(cmds[0])]
 
-        current = voice.get_volume()
-        target = float(cmds[1])
-        
-        if target == current:
-            return
+        try:
+            current = channel[0].get_volume()
+            target = float(cmds[1])
+            
+            if target == current:
+                return
 
-        diff = target - current
-        diff /= 100.0
+            diff = target - current
+            diff /= 100.0
 
-        for i in range(100):
-            current += diff 
-            voice.set_volume(current)
-            dsp.delay(0.01)
+            for i in range(100):
+                current += diff 
+                channel[0].set_volume(current)
+                dsp.delay(0.01)
+        except AttributeError:
+            pass
 
-    def do_info(self, cmd):
+        self.do_i()
+    
+
+    def do_i(self, cmd=[]):
+        print
+        for i, channel in enumerate(self.channels):
+            t = channel[1]
+            p = channel[2]
+            try:
+                print t, 'C:', i, 'V:', channel[0].get_volume(), 'L:', channel[0].get_sound().get_length(), 'P:', p
+            except AttributeError:
+                pass
+        print
+
         for i, voice in enumerate(self.voices):
-            print 'Voice:', i, 'Volume:', voice.get_volume(), 'Length:', voice.get_length()
+            print 'Available:', i, 'Length:', voice[0].get_length()
+
         print
 
     def default(self, cmd):
@@ -73,12 +124,11 @@ class Pippi(cmd.Cmd):
             self.m.mixer.quit()
             print 'Closing ALSA connection'
 
-        return True
-
 class Voice(threading.Thread):
-    def __init__(self, play, args, renders, voices, condition):
+    def __init__(self, play, type, args, renders, voices, condition):
         threading.Thread.__init__(self)
         self.play = play
+        self.type = type
         self.args = args
         self.voices = voices
         self.renders = renders
@@ -86,15 +136,16 @@ class Voice(threading.Thread):
 
     def run(self):
         self.condition.acquire()
-        self.renders += [ self.play(self.args) ]
+        self.renders += [ (self.play(self.args), self.type, self.args) ]
         self.condition.notify()
         self.condition.release()
 
 class Mixer(threading.Thread):
-    def __init__(self, mixer, renders, voices, condition):
+    def __init__(self, mixer, renders, voices, channels, condition):
         threading.Thread.__init__(self)
         self.condition = condition
         self.voices = voices
+        self.channels = channels
         self.renders = renders
         self.mixer = mixer
 
@@ -108,11 +159,10 @@ class Mixer(threading.Thread):
                     if self.mixer.get_init() is None:
                         self.mixer.init(frequency=44100, size=-16, channels=2)
 
-                    s = self.mixer.Sound(render)
-                    s.play(-1)
+                    s = self.mixer.Sound(render[0])
+                    self.channels.append((s.play(-1), render[1], render[2]))
 
                     self.renders.remove(render)
-                    self.voices.append(s)
 
             self.condition.release()
 
--- orc/shine.py
+++ orc/shine.py
@@ -2,22 +2,23 @@ import dsp
 import tune
 
 def play(args):
-    length = dsp.stf(3)
+    length = dsp.stf(dsp.rand(5, 10))
     volume = 0.2 
-    octave = 3
+    octave = 2 
     note = 'd'
-    reps = 30 
-    nlen = dsp.mstf(5000)
+    quality = tune.major
+    reps = dsp.randint(3, 11)
+    glitch = False
+    superglitch = False
+    pulse = False
 
     rhodes = dsp.read('sounds/220rhodes.wav').data
 
-    scale = [1,3,5,4]
+    #scale = [1,3,5,4]
+    scale = [1,5,8,6]
 
     for arg in args:
         a = arg.split(':')
-
-        if a[0] == 'b':
-            nlen = dsp.mstf(float(a[1]))
 
         if a[0] == 't':
             length = dsp.stf(float(a[1]))
@@ -31,24 +32,62 @@ def play(args):
         if a[0] == 'n':
             note = a[1]
 
+        if a[0] == 'q':
+            if a[1] == 'M':
+                quality = tune.major
+            elif a[1] == 'm':
+                quality = tune.minor
+            else:
+                quality = tune.major
+
         if a[0] == 'r':
             reps = int(a[1])
+
+        if a[0] == 'p':
+            pulse = True
+
+        if a[0] == 'g':
+            glitch = True
+
+        if a[0] == 'gg':
+            glitch = True
+            superglitch = True
 
         if a[0] == 's':
             scale = [int(s) for s in a[1].split('.')]
 
     out = ''
     for i in range(reps):
-        freq = tune.step(i, note, octave, scale)
+        freq = tune.step(i, note, octave, scale, quality)
         diff = freq / 440.0
 
         n = dsp.transpose(rhodes, diff)
-        n = dsp.fill(n, nlen)
+        n = dsp.fill(n, length)
 
-        o = [dsp.tone(nlen, freq * i * 0.5) for i in range(4)]
+        o = [dsp.tone(length, freq * i * 0.5) for i in range(4)]
         o = [dsp.env(oo) for oo in o]
         o = [dsp.pan(oo, dsp.rand()) for oo in o]
         o = dsp.mix([dsp.amp(oo, dsp.rand(0.05, 0.1)) for oo in o])
         out += dsp.mix([n, o])
 
-    return dsp.cache(out)
+    if glitch == True:
+        if superglitch == True:
+            mlen = dsp.mstf(100)
+        else:
+            mlen = dsp.flen(out) / 8
+
+        out = dsp.vsplit(out, dsp.mstf(1), mlen)
+        out = [dsp.pan(o, dsp.rand()) for o in out]
+        out = ''.join(dsp.randshuffle(out))
+
+    if pulse == True:
+        plen = dsp.mstf(dsp.rand(80, 500))
+        out = dsp.split(out, plen)
+        mpul = len(out) / dsp.randint(4, 8)
+
+        out = [dsp.env(o) * mpul for i, o in enumerate(out) if i % mpul == 0]
+        opads = dsp.wavetable('sine', len(out), dsp.rand(plen * 0.25, plen))
+        out = [dsp.pad(o, 0, int(opads[i])) for i, o in enumerate(out)]
+        out = dsp.env(''.join(out))
+
+    return dsp.cache(dsp.amp(out, volume))
